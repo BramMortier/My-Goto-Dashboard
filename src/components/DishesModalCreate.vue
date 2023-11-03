49196f59f0b0e172d4af6c2dafd6b36a12e6b79c
Bram Mortier <79015250+BramMortier@users.noreply.github.com>
2023-11-03 07:15:20 +0100

    Added validation rules to the create dish form

--- src/components/DishesModalCreate.vue
+++ src/components/DishesModalCreate.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Form } from "vee-validate";
+import * as yup from "yup";
 
 import BaseFormFileUploader from "@components/BaseFormFileUploader.vue";
 import BaseFormInput from "@components/BaseFormInput.vue";
@@ -8,13 +9,24 @@ import BaseFormRow from "@components/BaseFormRow.vue";
 import BaseButton from "@components/BaseButton.vue";
 import BaseFormCheckbox from "@components/BaseFormCheckbox.vue";
 
+const dishesFormValidationSchema = yup.object({
+  dishThumbnail: yup.string().required("Dish Thumbnail is required"),
+  dishName: yup.string().required("Dish Name is required"),
+  description: yup.string().required("Description is required"),
+  foodCategory: yup.string().required("Food Category is required"),
+});
+
 const handleDishesFormSubmit = async (values) => {
   console.log(values);
 };
 </script>
 
 <template>
-  <Form class="dishes-form-create" @submit="handleDishesFormSubmit">
+  <Form
+    class="dishes-form-create"
+    :validationSchema="dishesFormValidationSchema"
+    @submit="handleDishesFormSubmit"
+  >
     <h3>Add a new Dish</h3>
     <BaseFormFieldset label="Dish thumbnail">
       <BaseFormFileUploader
